85a53e885fa488d429465983556526995203a9ea
Peter LaFontaine <lafonta@bu.edu>
2017-08-04 22:04:07 -0400

    finished 0-2

--- 01_temperature/temperature.rb
+++ 01_temperature/temperature.rb
@@ -1,1 +1,9 @@
 #write your code here
+
+def ftoc(fahr)
+  (fahr - 32) * (5.0/9.0)
+end
+
+def ctof(cent)
+  (cent*1.8) + 32
+end
--- 02_calculator/calculator.rb
+++ 02_calculator/calculator.rb
@@ -1,1 +1,37 @@
 #write your code here
+
+def add(a,b)
+  a+b
+end
+
+def subtract(c,d)
+  c-d
+end
+
+def sum(e)
+  total = 0
+  for i in e
+    total += i
+  end
+  total
+end
+
+def multiply(f)
+  total = 1
+  for i in f
+    total *= i
+  end
+  total
+end
+
+def power(h,i)
+  h**i
+end
+
+def factorial(n)
+  if n<=1
+    1
+  else
+    n * factorial(n-1)
+  end
+end
